2c756461591fe7815149f69f0e06082ba652f552
Ronald M. Clifford <roncli@roncli.com>
2021-05-02 16:01:18 -0700

    Increase download limit by 20x.

--- node/index.js
+++ node/index.js
@@ -225,10 +225,10 @@ class Index {
                 downloads[ip].count = 0;
             }
 
-            if (downloads[ip].count >= 50) {
+            if (downloads[ip].count >= 1000) {
                 // Send a 429 if they've been downloading too much.
                 res.status(429);
-                res.write("429 Too many requests, you are limited to 50 downloads in a 12 hour period.  Please contact roncli@roncli.com if you need to exceed this limit.");
+                res.write("429 Too many requests, you are limited to 1000 downloads in a 12 hour period.  Please contact roncli@roncli.com if you need to exceed this limit.");
                 res.end();
             } else {
                 // Update the download count.
