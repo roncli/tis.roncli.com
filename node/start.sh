fdd7f94ea0e4475579a36094c6da743bbde3a5f1
Ronald M. Clifford <roncli@roncli.com>
2021-08-24 13:16:56 -0700

    Allow for azure file storage to be optional.

--- node/start.sh
+++ node/start.sh
@@ -6,8 +6,12 @@ then
     echo "Warning: Application Insights is not setup.  Application will log to console."
 fi
 
-# Mount Azure Storage file share.
-mount -t cifs $(cat $FILES_URI) /mnt/files -o vers=3.0,username=$(cat $FILES_USERNAME),password=$(cat $FILES_PASSWORD),dir_mode=0755,file_mode=0755,serverino
+if [ $USE_AZURE_FILE_STORAGE -eq 1 ];
+then
+    # Mount Azure Storage file share.
+    mount -t cifs $(cat $FILES_URI) /mnt/files -o vers=3.0,username=$(cat $FILES_USERNAME),password=$(cat $FILES_PASSWORD),dir_mode=0755,file_mode=0755,serverino
+fi
+
 cd /var/www
 
 # Run app.
